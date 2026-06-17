b20e1152d061412470fda17231ee27b1c84b5401
Athul T S <athulsethumadhavan@gmail.com>
2026-06-17 06:55:22 +0300

    add privacy policy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.kotlin.dsl.release
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -5,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.atsIOSDev.kidWrite.kid_write"
+    namespace = "com.atsIOSDev.kidWrite"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +20,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.atsIOSDev.kidWrite.kid_write"
+        applicationId = "com.atsIOSDev.kidWrite"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -24,12 +28,24 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+    val keystoreProperties = Properties()
+    val keystorePropertiesFile = rootProject.file("local.properties")
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    }
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as? String
+            keyPassword = keystoreProperties["keyPassword"] as? String
+            storeFile = file("/Users/athulappstation/Desktop/Personal/Project/KidWrite/android/kid_write-keystore.jks")
+            storePassword = keystoreProperties["storePassword"] as? String
+        }
+    }
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
--- android/app/src/main/kotlin/com/atsIOSDev/kidWrite/kid_write/MainActivity.kt
+++ android/app/src/main/kotlin/com/atsIOSDev/kidWrite/kid_write/MainActivity.kt
@@ -1,4 +1,5 @@
-package com.atsIOSDev.kidWrite.kid_write
+package com.atsIOSDev.kidWrite
+
 
 import io.flutter.embedding.android.FlutterActivity
 
